edf5e5fd14f49fec4d51a3668a2891e8ceca1684
xukang hong <hong.xkang@gmail.com>
2022-08-26 21:58:44 +1200

    mvp-1

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import { Link } from "react-router-dom"
 import TcmAPI from "./TcmAPI"
 import Restock from "./Restock"
+import Analytics from "./Analytics"
 
 function Home() {
   return (
     <>
       <TcmAPI />
       <Restock />
+      <Analytics />
     </>
   )
 }
