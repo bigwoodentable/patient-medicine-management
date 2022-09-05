724a2b7af99a38b15efd611f5f7b5aefa639a119
xukang hong <hong.xkang@gmail.com>
2022-09-05 14:00:16 +1200

    h fix

--- server/db/migrations/20220530091405_reports.js
+++ server/db/migrations/20220530091405_reports.js
@@ -11,7 +11,7 @@ exports.up = function (knex) {
     table.string('diagnosis')
     table.decimal('total_costs', 8, 2)
     table.decimal('total_profit', 8, 2)
-    table.integer('patient_id').references('patients.id')
+    table.string('patient_id').references('patient_id').inTable('patients')
   })
 }
 
