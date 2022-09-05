0060b4484545ba293d24486f88eee3134dd51feb
xukang hong <hong.xkang@gmail.com>
2022-09-05 14:12:49 +1200

    h fix

--- server/db/seeds/prescriptions.js
+++ server/db/seeds/prescriptions.js
@@ -9,26 +9,26 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('prescriptions').insert([
-        {
-          prescribed_quantity: 35,
-          med_name: '百合',
-          report_id: 1,
-        },
-        {
-          prescribed_quantity: 20,
-          med_name: '白花蛇舌草',
-          report_id: 1,
-        },
-        {
-          prescribed_quantity: 15,
-          med_name: '白扁豆',
-          report_id: 2,
-        },
-        {
-          prescribed_quantity: 15,
-          med_name: '敗醬草',
-          report_id: 2,
-        },
+        // {
+        //   prescribed_quantity: 35,
+        //   med_name: '百合',
+        //   report_id: 1,
+        // },
+        // {
+        //   prescribed_quantity: 20,
+        //   med_name: '白花蛇舌草',
+        //   report_id: 1,
+        // },
+        // {
+        //   prescribed_quantity: 15,
+        //   med_name: '白扁豆',
+        //   report_id: 2,
+        // },
+        // {
+        //   prescribed_quantity: 15,
+        //   med_name: '敗醬草',
+        //   report_id: 2,
+        // },
       ])
     })
 }
