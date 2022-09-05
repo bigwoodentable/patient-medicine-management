c5e359135c5882bd6305e463256370b8deda4b41
xukang hong <hong.xkang@gmail.com>
2022-09-05 15:01:11 +1200

    h fix

--- server/db/migrations/20220530091405_reports.js
+++ server/db/migrations/20220530091405_reports.js
@@ -3,15 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('reports', (table) => {
-    table.increments('report_id').primary()
-    table.date('date_added')
-    table.decimal('prescription_price', 8, 2)
-    table.decimal('prescription_number', 8, 2)
-    table.string('diagnosis')
-    table.decimal('total_costs', 8, 2)
-    table.decimal('total_profit', 8, 2)
-    table.string('patient_id').references('patient_id').inTable('patients')
+  return knex.schema.createTable("reports", (table) => {
+    table.increments("report_id").primary()
+    table.date("date_added")
+    table.decimal("prescription_price", 8, 2)
+    table.decimal("prescription_number", 8, 2)
+    table.string("diagnosis")
+    table.decimal("total_costs", 8, 2)
+    table.decimal("total_profit", 8, 2)
+    table.string("patient_id").references("patients.patient_id")
+    // table.string('patient_id').references('patient_id').inTable('patients')
   })
 }
 
@@ -20,5 +21,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable('reports')
+  return knex.schema.dropTable("reports")
 }
