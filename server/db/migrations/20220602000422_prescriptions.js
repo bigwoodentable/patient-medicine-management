9a144727fcc6eef51906afba56aa394dd577ecad
xukang hong <hong.xkang@gmail.com>
2022-09-05 15:10:24 +1200

    h fix

--- server/db/migrations/20220602000422_prescriptions.js
+++ server/db/migrations/20220602000422_prescriptions.js
@@ -6,12 +6,12 @@ exports.up = function (knex) {
   return knex.schema.createTable("prescriptions", (table) => {
     table.increments().primary()
     table.integer("prescribed_quantity")
-    // table.string('med_name')
-    // table.integer('report_id')
-    // table.string("med_name").references("med_name").inTable("medicines")
+    table.string("med_name")
+    table.integer("report_id")
+    // table.string("med_name").references("med_name").inTable("stocks")
     // table.integer("report_id").references("report_id").inTable("reports")
-    table.string("med_name").references("medicines.med_name")
-    table.integer("report_id").references("reports.report_id")
+    // table.string("med_name").references("medicines.stocks")
+    // table.integer("report_id").references("reports.report_id")
   })
 }
 
