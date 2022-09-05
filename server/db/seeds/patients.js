4032a599fbca74276ffae1e039864675d22b690f
xukang hong <hong.xkang@gmail.com>
2022-09-05 15:38:42 +1200

    h fix

--- server/db/seeds/patients.js
+++ server/db/seeds/patients.js
@@ -4,37 +4,37 @@
  */
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('patients')
+  return knex("patients")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('patients').insert([
+      return knex("patients").insert([
         {
-          patient_id: '1',
-          fname: 'mike',
-          lname: 'lee',
+          patient_id: "1",
+          fname: "mike",
+          lname: "lee",
           age: 51,
-          notes: 'male, architect',
-          status: 'active',
-          date_added: '2018-11-30',
+          notes: "male, architect",
+          status: "active",
+          date_added: "2018-11-30",
         },
         {
-          patient_id: '2',
-          fname: 'john',
-          lname: 'chan',
+          patient_id: "2",
+          fname: "john",
+          lname: "chan",
           age: 31,
-          notes: 'male, doctor',
-          status: 'deleted',
-          date_added: '2018-11-30',
+          notes: "male, doctor",
+          status: "deleted",
+          date_added: "2018-11-30",
         },
         {
-          patient_id: '3',
-          fname: 'james',
-          lname: 'wong',
+          patient_id: "3",
+          fname: "james",
+          lname: "wong",
           age: 55,
-          notes: 'male, lawyer',
-          status: 'active',
-          date_added: '2018-11-30',
+          notes: "male, lawyer",
+          status: "active",
+          date_added: "2018-11-30",
         },
       ])
     })
--- server/db/seeds/reports.js
+++ server/db/seeds/reports.js
@@ -4,40 +4,40 @@
  */
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('reports')
+  return knex("reports")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('reports').insert([
+      return knex("reports").insert([
         {
           report_id: 1,
-          date_added: '3/12/2019',
-          diagnosis: 'stomach ache',
+          date_added: "3/12/2019",
+          diagnosis: "stomach ache",
           prescription_price: 10,
           prescription_number: 7,
           total_costs: 10,
           total_profit: 7,
-          patient_id: '1',
+          patient_id: "1",
         },
         {
           report_id: 2,
-          date_added: '2/04/2022',
-          diagnosis: 'head ache',
+          date_added: "2/04/2022",
+          diagnosis: "head ache",
           prescription_price: 15.5,
           prescription_number: 3,
           total_costs: 10,
           total_profit: 7,
-          patient_id: '2',
+          patient_id: "2",
         },
         {
           report_id: 3,
-          date_added: '7/02/2008',
-          diagnosis: 'heart ache',
+          date_added: "7/02/2008",
+          diagnosis: "heart ache",
           prescription_price: 12,
           prescription_number: 5,
           total_costs: 10,
           total_profit: 7,
-          patient_id: '2',
+          patient_id: "2",
         },
       ])
     })
